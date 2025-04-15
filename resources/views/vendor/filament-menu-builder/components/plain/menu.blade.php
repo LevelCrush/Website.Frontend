a2c32e9b7b3c8b634fd540618f9ae46704b88e6f
Primal-13 <primal@levelcrush.com>
2025-04-15 07:47:32 -0400

    add null check in vendor view for menus

--- resources/views/vendor/filament-menu-builder/components/plain/menu.blade.php
+++ resources/views/vendor/filament-menu-builder/components/plain/menu.blade.php
@@ -1,5 +1,7 @@
 <ul>
-    @foreach($menuItems as $menuItem)
-        @include('filament-menu-builder::components.plain.menu-item', ['item' => $menuItem])
-    @endforeach
+    @if(!empty($menuItems))
+        @foreach($menuItems as $menuItem)
+            @include('filament-menu-builder::components.plain.menu-item', ['item' => $menuItem])
+        @endforeach
+    @endif
 </ul>
