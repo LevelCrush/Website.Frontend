4a5c0edc5c5fbb9670fbdcd6dbf0767f9a554d0c
Primal-13 <primal@levelcrush.com>
2025-04-17 00:31:45 -0400

    Null guard

--- resources/views/vendor/filament-menu-builder/components/offcanvas/menu.blade.php
+++ resources/views/vendor/filament-menu-builder/components/offcanvas/menu.blade.php
@@ -1,30 +1,32 @@
 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-    @foreach($menuItems as $menuItem)
+    @if(!empty($menuItems))
+        @foreach($menuItems as $menuItem)
 
-        @if($menuItem->parameters->isNotEmpty() && isset($menuItem->parameters['guest']))
-            @auth
-                @continue
-            @endauth
-        @endif
-
-        @if($menuItem->parameters->isNotEmpty() && isset($menuItem->parameters['member']))
-            @guest
-                @continue
-            @endguest
-        @endif
-
-        @if($menuItem->parameters->isNotEmpty() && isset($menuItem->parameters['admin']))
-
-            @guest
-                @continue
-            @endguest
-
-            @if(session('metadata',['discord' => ['isAdmin' => false]])['discord']['isAdmin'] === false)
-                @continue
+            @if($menuItem->parameters->isNotEmpty() && isset($menuItem->parameters['guest']))
+                @auth
+                    @continue
+                @endauth
             @endif
 
-        @endif
+            @if($menuItem->parameters->isNotEmpty() && isset($menuItem->parameters['member']))
+                @guest
+                    @continue
+                @endguest
+            @endif
 
-        @include('filament-menu-builder::components.offcanvas.menu-item', ['item' => $menuItem])
-    @endforeach
+            @if($menuItem->parameters->isNotEmpty() && isset($menuItem->parameters['admin']))
+
+                @guest
+                    @continue
+                @endguest
+
+                @if(session('metadata',['discord' => ['isAdmin' => false]])['discord']['isAdmin'] === false)
+                    @continue
+                @endif
+
+            @endif
+
+            @include('filament-menu-builder::components.offcanvas.menu-item', ['item' => $menuItem])
+        @endforeach
+    @endif
 </ul>
