fcab2b39fc7c76adb13c9bd8e3d29922007d1642
Primal-13 <primal@levelcrush.com>
2025-05-16 22:45:33 -0400

    Hook dashboard menu to cms

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -6,12 +6,12 @@
     @vite(['resources/js/dashboard.js'])
     <div class="container">
         <div class="row">
-            <div class="col-12 col-md-6 border-end-md border-bottom border-bottom-md-0">
+            <div class="col-12 col-md-3 border-end-md border-bottom border-bottom-md-0">
                 <div class="container">
-                    <h2>Dashboard menu</h2>
+                    <x-filament-menu-builder::menu slug="dashboard-navigation" view="filament-menu-builder::components.bootstrap5.menu"/>
                 </div>
             </div>
-            <div class="col-12 col-md-6">
+            <div class="col-12 col-md">
                 <div class="container">
                     @yield('dashboard')
                 </div>
