92f69d530e6f04a6351ebe1eb2e97fe7df337254
Primal-13 <primal@levelcrush.com>
2025-05-17 03:14:47 -0400

    Disable registration and authentication routes

--- routes/auth.php
+++ routes/auth.php
@@ -4,6 +4,9 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
 
+
+
+/*
 Route::middleware('guest')->group(function () {
     Volt::route('login', 'auth.login')
         ->name('login');
@@ -29,7 +32,7 @@ Route::middleware('auth')->group(function () {
 
     Volt::route('confirm-password', 'auth.confirm-password')
         ->name('password.confirm');
-});
+}); */
 
 Route::post('logout', App\Livewire\Actions\Logout::class)
     ->name('logout');
