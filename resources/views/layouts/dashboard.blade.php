96bcd13609a47b76766ee855f2fddb4b46faf9bd
Primal-13 <primal@levelcrush.com>
2025-05-16 22:35:16 -0400

    add container class to dashboard columns

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -7,10 +7,14 @@
     <div class="container">
         <div class="row">
             <div class="col-12 col-md-6 border-end-md border-bottom border-bottom-md-0">
-                <h2>Dashboard menu</h2>
+                <div class="container">
+                    <h2>Dashboard menu</h2>
+                </div>
             </div>
             <div class="col-12 col-md-6">
-                @yield('dashboard')
+                <div class="container">
+                    @yield('dashboard')
+                </div>
             </div>
         </div>
   </div>
