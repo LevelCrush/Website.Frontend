bf64d93a252e52f3e73e402735f439e7311ddef6
Primal-13 <primal@levelcrush.com>
2025-05-16 21:28:48 -0400

    Removed unused settings route

--- routes/web.php
+++ routes/web.php
@@ -26,14 +26,6 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/dashboard', [\App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard');
 });
 
-Route::middleware(['auth'])->group(function () {
-    Route::redirect('settings', 'settings/profile');
-
-    Volt::route('settings/profile', 'settings.profile')->name('settings.profile');
-    Volt::route('settings/password', 'settings.password')->name('settings.password');
-    Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
-});
-
 // fallback routes
 
 
