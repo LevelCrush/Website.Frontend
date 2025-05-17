5df5023dd0c2ac207ef5b1e7a1b0b6ca68ea9d72
Primal-13 <primal@levelcrush.com>
2025-05-17 02:05:22 -0400

    Added skeleton functions for orders and integrations

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,7 +6,22 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         return view('pages.dashboard.index');
+    }
+
+    public function orders()
+    {
+
+        $orders = [];
+
+        return view('pages.dashboard.order')->with('orders', $orders);
+    }
+
+    public function integrations()
+    {
+        $integrations = [];
+        return view('pages.dashboard.integrations')->with('integrations', $integrations);
     }
 }
